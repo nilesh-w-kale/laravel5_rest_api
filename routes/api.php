71b266258104b629281067704b7817a425927eb1
Nilesh Kale <nilesh.w.kale@gmail.com>
2019-04-10 19:29:48 +0530

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +12,16 @@ use Illuminate\Http\Request;
 |
 */
 
+/* Default Route*/
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix'=>'product'],function() {
+	
+	Route::apiResource('{product}/reviews','ReviewController');
 });
