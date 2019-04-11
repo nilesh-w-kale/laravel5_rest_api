4913525fad3c3121e4d69b0113d36802b1ab1723
Nilesh Kale <nilesh.w.kale@gmail.com>
2019-04-11 23:52:03 +0530

    Product Resource Created

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ProductResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        /*
+        return parent::toArray($request);
+        */
+        return [
+            'name' => $this->name,
+            'detail' => $this->detail,
+            'price'=> $this->price,
+            'stock' => $this->stock,
+            'discount'=> $this->discount
+        ];
+    }
+}
