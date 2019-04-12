f0599ef28ee7b11f9a1344daeae974e80b78c247
Nilesh Kale <nilesh.w.kale@gmail.com>
2019-04-12 17:47:05 +0530

    Product Destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -115,6 +115,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+       return Response([
+        null
+       ], Response::HTTP_NO_CONTENT);
     }
 }
